1824baab0d462052052690b5c74190ee16044491
Michael Saunby <mike@saunby.net>
2021-09-15 11:44:56 +0100

    Update app.py

--- soil-moisture-sensor/app.py
+++ soil-moisture-sensor/app.py
@@ -1,7 +1,7 @@
 from counterfit_connection import CounterFitConnection
 CounterFitConnection.init('127.0.0.1', 5000)
 
-import time
+zimport time
 from counterfit_shims_grove.adc import ADC
 from counterfit_shims_grove.grove_relay import GroveRelay
 import json
